ad0ab00535ce407bb23a1e7a64ed3c23e7744b8f
Lee Parks <lcp3.dev@gmail.com>
2021-03-18 03:08:28 -0400

    Removed the largely unnecessary Vector3.cross checks after realizing the collision's contact data could be used (Vector2 hit).  Adjusted appropriately and have a different solution for basic corner collision.  Still work to be done for edge cases of corner interactions.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,73 +10,35 @@ public class Ball : MonoBehaviour
     Rigidbody2D _rigidbody2D;
     float _directionX;
     float _directionY;
-    private float _crossX;
-    private float _crossY;
-    private Vector2 _mousePosition;
-    private Vector2 localPoint;
-    private Transform _ballTransform;
 
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider == null)
-            return;
-
-        localPoint = _ballTransform.InverseTransformPoint(collision.GetContact(0).point); // Getting the point of collision
-        Debug.Log($"{_ballTransform.name} {localPoint}"); //Ideally returning an X *or* a Y, e.g. {1,0} or {0,-1}
-
-
-        _collisionX = Mathf.Round(localPoint.x * 10) / 10; //Rounding collision points to nearest tenth
-        _collisionY = Mathf.Round(localPoint.y * 10) / 10;
-            
-        Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.green);// Visualize the contact point
-        
-
-        Debug.Log(_collisionX);
-        Debug.Log(_collisionY);
-
-        if (_collisionX != 0) 
-        {
-            _directionX *= -1; //Flip X velocity
-            _rigidbody2D.velocity = new Vector2(_directionX, _directionY).normalized;
-        }
-        if (_collisionY != 0) 
-        {
-            _directionY *= -1; //Flip Y velocity
-            _rigidbody2D.velocity = new Vector2(_directionX, _directionY).normalized;
-        }
-
-    }*/
-
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 hit = collision.GetContact(0).normal;
+        Vector2 hit = -collision.GetContact(0).normal;
 
         float angle = Vector2.Angle(hit, Vector2.up);
-        Vector3 cross = Vector3.Cross(Vector3.forward, hit);
-        Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.green);// Visualize the contact point
-        //ConsoleDebug(hit, angle, cross);
-        _crossX = Mathf.Round(cross.x * 10) / 10; //Rounding collision points to nearest tenth
-        _crossY = Mathf.Round(cross.y * 10) / 10;
+        //Vector3 cross = Vector3.Cross(Vector3.forward, hit);
+        RunDebug(collision, hit, angle); //Uncomment for Debug Raycast + Console info
 
-        //print($"{_crossX} X, {_crossY} Y");
-        if (Mathf.Approximately(angle, 0))
+
+        if (Mathf.Approximately(angle, 180))
         {
-            if (cross.x < 0) //Bottom hit
+            if (hit.y < 0) //Bottom hit
             {
-                _directionY *= -1;
+                _directionY *= -1; //Flip Y velocity
                 ChangeVelocity();
                 print("Hit Bottom");
             }
         }
         else if (Mathf.Approximately(angle, 90))
         {
-            if (cross.y < 0)
+            if (hit.x > 0)
             {
                 _directionX *= -1; //Flip X velocity
                 ChangeVelocity();
                 print("Hit Right");
             }
-            if (cross.y > 0)
+            if (hit.x < 0)
             {
                 _directionX *= -1; //Flip X velocity
                 ChangeVelocity();
@@ -84,31 +46,32 @@ public class Ball : MonoBehaviour
             }
 
         }
-        else if (Mathf.Approximately(angle, 180))
+        else if (Mathf.Approximately(angle, 0))
         {
-            if (cross.x > 0) //Top hit
+            if (hit.y > 0) //Top hit
             {
-                _directionY *= -1;
+                _directionY *= -1; //Flip Y velocity
                 ChangeVelocity();
                 print("Hit Top");
             }
         }
-        else if (Mathf.Abs(cross.x) == Mathf.Abs(cross.x))
+        else if (angle >= 1 && angle <= 89 || angle >= 91 && angle <= 179) //Corner check
         {
             _directionX *= -1; //Flip X velocity
-            _directionY *= -1; //Flip X velocity
+            _directionY *= -1; //Flip Y velocity
             ChangeVelocity();
             print("Hit Corner");
         }
     }
 
-    private static void ConsoleDebug(Vector2 hit, float angle, Vector3 cross)
+    private static void RunDebug(Collision2D collision, Vector2 hit, float angle)
     {
+        Debug.DrawRay(collision.contacts[0].point, collision.contacts[0].normal, Color.green);// Visualize the contact point
         print($"Hit: {hit}, Hit.x: {hit.x}, Hit.y: {hit.y}");
         print(angle);
-        print($"Cross: {cross}, Cross.x: {cross.x}, Cross.y: {cross.y}");
+        /*print($"Cross: {cross}, Cross.x: {cross.x}, Cross.y: {cross.y}");
         print($"Abs X: {Mathf.Abs(cross.x)} Abs Y: {Mathf.Abs(cross.x)}");
-        print($"== check {Mathf.Abs(cross.x) == Mathf.Abs(cross.x)}");
+        print($"== check {Mathf.Abs(cross.x) == Mathf.Abs(cross.x)}");*/
     }
 
     private void ChangeVelocity()
@@ -119,7 +82,6 @@ public class Ball : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _ballTransform = GetComponent<Transform>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _directionX = _ballSpeed;
         _directionY = _ballSpeed;
