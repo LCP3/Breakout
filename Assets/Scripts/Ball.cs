4a6a7b150d84fe9273a007fd6d77d0a23cbe4a8d
Lee Parks <lcp3.dev@gmail.com>
2021-03-19 20:38:47 -0400

    Added TMPro, setup for UI elements (Lives, Score), sliced paddle for scalable width.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -9,24 +9,24 @@ public class Ball : MonoBehaviour
     float _directionY;
     private Vector2 _velocity;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>(); //Cache
+        _directionX = _ballSpeed;
+        _directionY = _ballSpeed;
+
+        _velocity = new Vector2(_directionX, _directionY); //Set up velocity for initial use
+        _rigidbody2D.velocity = _velocity; //Set ball's starting velocity
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //On collision
     {
         BounceBall(_rigidbody2D, collision.contacts[0].normal);
     }
 
     private void BounceBall(Rigidbody2D rigidbody2D, Vector2 normal)
     {
-        _velocity = Vector2.Reflect(_velocity, normal);
-        _rigidbody2D.velocity = _velocity;
-    }
-
-    void Start()
-    {
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _directionX = _ballSpeed;
-        _directionY = _ballSpeed;
-
-        _velocity = new Vector2(_directionX, _directionY);
-        _rigidbody2D.velocity = _velocity;
+        _velocity = Vector2.Reflect(_velocity, normal); //Reflect the current velocity, reflection at contact[0].normal
+        _rigidbody2D.velocity = _velocity; //Set new reflected velocity
     }
 }
